47cd175001b8bb531e007c4f30b4ec7fd4512071
Nicholas Narsing <soren121@sorenstudios.com>
2017-10-09 10:55:42 -0400

    Improve Gulp compilation; add inline SVG test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
-const concat = require('gulp-concat');
+const concat = require('gulp-concat-util');
+const uglify = require('gulp-uglify');
 const injectSVG = require('gulp-inject-svg');
 const injectCSS = require('gulp-inject-css');
 
@@ -11,12 +12,16 @@ gulp.task('html', function () {
 });
 
 gulp.task('js', function () {
-    return gulp.src([
-        './node_modules/timeago.js/dist/timeago.min.js',
-        './node_modules/svg4everybody/dist/svg4everybody.min.js',
-        './node_modules/masonry-layout/dist/masonry.pkgd.min.js',
-        './node_modules/imagesloaded/imagesloaded.pkgd.min.js'
-    ]).pipe(concat('dist.js')).pipe(gulp.dest('./dist/'));
+    var libs = [
+        './node_modules/timeago.js/dist/timeago.js',
+        './node_modules/masonry-layout/dist/masonry.pkgd.js',
+        './node_modules/imagesloaded/imagesloaded.pkgd.js'
+    ];
+
+    return gulp.src(libs)
+        .pipe(concat.scripts('dist.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('default', ['html', 'js']);
